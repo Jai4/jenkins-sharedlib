610d9eb55de07617c9735cb012a1aff8bc2d600f
Jai Gowtham <jaigowtham147@gmail.com>
2020-12-03 14:44:47 -0600

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,9 +1,8 @@
 def call(String name = 'human') {
    echo "Hello, ${name}."
-   def dockerRunOpts = "--network host -v /var/run/docker.sock:/var/run/docker.sock --privileged=true"
+   def dockerRunOpts = "--network host"
    sh "docker pull docker"
    docker.image('docker').inside(dockerRunOpts){
-    sh "docker image ls"
     sh "ls"
   }
 }
