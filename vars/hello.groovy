543785802e59b05f93071b87fe00a31ab75135aa
Jai Gowtham <jaigowtham147@gmail.com>
2020-12-03 01:21:37 -0600

    use docker image to see docker in docker -v sock

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,8 +1,8 @@
 def call(String name = 'human') {
    echo "Hello, ${name}."
    def dockerRunOpts = "--network host -v /var/run/docker.sock:/var/run/docker.sock "
-   sh "docker pull ubuntu"
-   docker.image('ubuntu').inside(dockerRunOpts){
+   sh "docker pull docker"
+   docker.image('docker').inside(dockerRunOpts){
     sh "docker image ls"
     sh "ls"
   }
