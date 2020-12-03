bc59d1953b6808e256432904e50917f43fdb54e1
Jai Gowtham <jaigowtham147@gmail.com>
2020-12-03 01:19:28 -0600

    list docker images

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -3,7 +3,7 @@ def call(String name = 'human') {
    def dockerRunOpts = "--network host -v /var/run/docker.sock:/var/run/docker.sock "
    sh "docker pull ubuntu"
    docker.image('ubuntu').inside(dockerRunOpts){
-    
+    sh "docker image ls"
     sh "ls"
   }
 }
