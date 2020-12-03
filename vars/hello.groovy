2e4088841dce162b88c4c8df59819d8eb2ab595e
Jai Gowtham <jaigowtham147@gmail.com>
2020-12-02 23:53:20 -0600

    update script

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -2,7 +2,7 @@ def call(String name = 'human') {
    echo "Hello, ${name}."
    def dockerRunOpts = "--network host"
    sh "docker pull hello-world"
-   docker.image("hello-world").inside(dockerRunOpts){
+   docker.image('hello-world').inside(dockerRunOpts){
     sh "ls"
   }
 }
