ad08d8f37cf0cc11624922cd3f59c5cf33648b33
Jai Gowtham <jaigowtham147@gmail.com>
2020-12-04 17:37:41 -0600

    adding gradlew command

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -5,14 +5,18 @@ def call(String name = 'human') {
    def dockerRunOpts = "--network host -e GRADLE_USER_HOME=./.gradle"
    def BUILD_IMAGE = "507997576901.dkr.ecr.us-east-2.amazonaws.com/zulugradle:0.1"
    sh "docker pull 507997576901.dkr.ecr.us-east-2.amazonaws.com/zulugradle:0.1"
+   
    docker.image(BUILD_IMAGE).inside(dockerRunOpts){
    
       sh "pwd"
       sh "ls"
-      sh "cd ./"
-      sh "ls"
       sh "which gradle"
       sh "gradle --version"
+      
+      sh "./gradlew clean build"
   }
+   
+   echo "see whether u can see a build directory"
+   sh "ls"
 }
 
