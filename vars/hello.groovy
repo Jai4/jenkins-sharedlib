f7b1072816ee6e53439aaccf4aab468b67b50f4b
Jai Gowtham <jaigowtham147@gmail.com>
2020-12-03 19:32:22 -0600

    pwd of jenkins agent

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,11 +1,13 @@
 def call(String name = 'human') {
    echo "Hello, ${name}."
+   echo "Present working directory of my jenkins build is: " 
+   sh "pwd"
    def dockerRunOpts = "--network host -e GRADLE_USER_HOME=./.gradle"
    def BUILD_IMAGE = "507997576901.dkr.ecr.us-east-2.amazonaws.com/zulugradle:0.1"
    sh "docker pull 507997576901.dkr.ecr.us-east-2.amazonaws.com/zulugradle:0.1"
    docker.image(BUILD_IMAGE).inside(dockerRunOpts){
    
-    sh "pwd"
+      sh "pwd"
       sh "ls"
       sh "which gradle"
       sh "gradle --version"
