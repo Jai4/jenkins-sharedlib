496b08d9082b768596cb63144052aaf5949d8947
Jai Gowtham <jaigowtham147@gmail.com>
2020-12-03 19:12:44 -0600

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,6 +1,6 @@
 def call(String name = 'human') {
    echo "Hello, ${name}."
-   def dockerRunOpts = "--network host"
+   def dockerRunOpts = "--network host -e GRADLE_USER_HOME=./.gradle"
    def BUILD_IMAGE = "507997576901.dkr.ecr.us-east-2.amazonaws.com/zulugradle:0.1"
    sh "docker pull 507997576901.dkr.ecr.us-east-2.amazonaws.com/zulugradle:0.1"
    docker.image(BUILD_IMAGE).inside(dockerRunOpts){
