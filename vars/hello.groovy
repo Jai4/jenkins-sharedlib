0e9c85847a90ea17b7d5ff46f8a98d25cbbfa0f6
Jai Gowtham <jaigowtham147@gmail.com>
2020-12-04 22:08:41 -0600

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -3,7 +3,6 @@ def call(String name = 'human') {
    echo "Present working directory of my jenkins build is: " 
    sh "pwd"
    sh "ls"
-   sh "gradle --version"
    def dockerRunOpts = "--network host -e GRADLE_USER_HOME=./.gradle"
    def BUILD_IMAGE = "507997576901.dkr.ecr.us-east-2.amazonaws.com/zulugradle:0.1"
    sh "docker pull 507997576901.dkr.ecr.us-east-2.amazonaws.com/zulugradle:0.1"
