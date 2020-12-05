d02ba936d74417251031b9914417789828829b99
Jai Gowtham <jaigowtham147@gmail.com>
2020-12-04 21:56:28 -0600

    add ls command outside

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -2,6 +2,7 @@ def call(String name = 'human') {
    echo "Hello, ${name}."
    echo "Present working directory of my jenkins build is: " 
    sh "pwd"
+   sh "ls"
    def dockerRunOpts = "--network host -e GRADLE_USER_HOME=./.gradle"
    def BUILD_IMAGE = "507997576901.dkr.ecr.us-east-2.amazonaws.com/zulugradle:0.1"
    sh "docker pull 507997576901.dkr.ecr.us-east-2.amazonaws.com/zulugradle:0.1"
