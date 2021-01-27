aaa8be4125fa23a7f501d607bbe7a36b4983f6ec
Jai Muralidharan <jaigowtham_manickammuralidharan@trendmicro.com>
2021-01-27 12:31:51 -0600

    Correct docker command

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,6 +1,6 @@
 def call(String name = 'human') {
    
-   sh "docker create network --driver bridge united"
+   sh "docker network create --driver bridge united"
    def dockerRunOpts = "--network united --name ms -e GRADLE_USER_HOME=./.gradle"
    def BUILD_IMAGE = "507997576901.dkr.ecr.us-east-1.amazonaws.com/javagradlecurl:latest"
    sh "docker pull ${BUILD_IMAGE}"
