7ded88aed71efaf6904c662a9484350d98d4f238
Jai Muralidharan <jaigowtham_manickammuralidharan@trendmicro.com>
2021-01-27 12:10:32 -0600

    Add network

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,18 +1,20 @@
 def call(String name = 'human') {
    
-   def dockerRunOpts = "--network host -e GRADLE_USER_HOME=./.gradle"
-   def BUILD_IMAGE = "507997576901.dkr.ecr.us-east-2.amazonaws.com/zulugradle:latest"
-   sh "docker pull 507997576901.dkr.ecr.us-east-2.amazonaws.com/zulugradle:latest"
+   sh "docker create network --driver bridge united"
+   def dockerRunOpts = "--network united --name ms -e GRADLE_USER_HOME=./.gradle"
+   def BUILD_IMAGE = "507997576901.dkr.ecr.us-east-1.amazonaws.com/javagradlecurl:latest"
+   sh "docker pull ${BUILD_IMAGE}"
    
    
-   def localStackRunOpts = "--network host"
+   def localStackRunOpts = "--network united --name ls"
    def LOCALSTACK_IMAGE = "localstack/localstack"
-   sh "docker pull localstack/localstack"
+   sh "docker pull ${LOCALSTACK_IMAGE}"
   
   
    docker.image(LOCALSTACK_IMAGE).withRun(localStackRunOpts){
+      sleep 15
          docker.image(BUILD_IMAGE).inside(dockerRunOpts){
-            sh "./gradlew clean build --debug"
+            sh "curl http://ls:4566/health"
         }
    }
 }
