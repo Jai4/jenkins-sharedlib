9989c4899a3b47a9b4ce6348aecfebc9e1ad8744
Jai Gowtham <jaigowtham147@gmail.com>
2021-02-02 20:21:32 -0600

    adding debug

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -14,7 +14,7 @@ def call(String name = 'human') {
    try{
          docker.image(LOCALSTACK_IMAGE).withRun(localStackRunOpts){
                docker.image(BUILD_IMAGE).inside(dockerRunOpts){
-                  sh "./gradlew clean build"
+                  sh "./gradlew clean build --debug"
               }
          }
    } catch(Exception e){
